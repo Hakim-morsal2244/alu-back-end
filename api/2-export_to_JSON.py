1789655a8b70130dfdd5348eef65783e608d28ef
Hakim-morsal2244 <m.hakim@alustudent.com>
2025-02-13 16:44:02 +0000

    Task 2

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
-"""
-fetches data from an API
-and exports it to a JSON file
-"""
-
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    # Check if an argument is provided
-    if len(argv) < 2:
-        print("Usage: python3 2-export_to_JSON.py <user_id>")
-        exit(1)
-    
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(userId), verify=False).json()
-    username = user.get('username')
-    tasks = []
-    
-    for task in todo:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = username
-        tasks.append(task_dict)
-    
-    jsonobj = {}
-    jsonobj[userId] = tasks
-    
-    with open("{}.json".format(userId), 'w') as jsonfile:
-        json.dump(jsonobj, jsonfile)
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
